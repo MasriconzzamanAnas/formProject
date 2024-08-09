10ce9a8737a0655ea9a7fda12a32843b9f9dc4ae
Masricozzaman Anas <153457604+MasriconzzamanAnas@users.noreply.github.com>
2024-08-09 07:32:59 +0600

    age calculator

---
+++ script.js
@@ -0,0 +1,33 @@
+let age = document.querySelector('#from').addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const fname= document.querySelector('#fname').value;
+    const lname= document.querySelector('#lname').value;
+    const email= document.querySelector('#email').value;
+    const phone= document.querySelector('#phone').value;
+    const dob = document.querySelector('#dob').value;
+    const gender = document.querySelector('#gender').value;
+    const occou= document.querySelector('#occu').value;
+
+    class student{
+        constructor(fname,lname,email,phone,dob,gender,occou){
+            this.fname=fname;
+            this.lname=lname;
+            this.email=email;
+            this.phone=phone;
+            this.dob=dob;
+            this.gender=gender;
+            this.occou=occou;
+            this.age = ''
+        }
+        ageCalculator(){
+            if(this.age= ''){
+                let birth= Date.now(this.dob);
+                let timeDeff= Date.now() - birth.getTime();
+                let ageInMillisecond= new Date(timeDeff);
+                let ageInYear= Math.abs(ageInMillisecond.getUTCFullYear() - 1970)
+                this.age= ageInYear
+            }
+            return this.age
+        }
+    }
+})
